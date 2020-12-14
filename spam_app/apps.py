f820a872845195ad5da237085a14644148ee0d58
antonellaschiavoni <antonellaschiavoni2@gmail.com>
2020-12-14 15:26:43 -0300

    Fix model path

--- spam_app/apps.py
+++ spam_app/apps.py
@@ -1,9 +1,11 @@
 from django.apps import AppConfig
 import joblib
 import os
+import logging
 
 
 class SpamAppConfig(AppConfig):
     name = 'spam_app'
-    model_path = os.path.abspath(os.path.join('spam-model', 'pipe_model.pkl'))
+    logging.info(f"Current Path: {os.getcwd}")
+    model_path = os.path.abspath(os.path.join('spam_app', 'spam-model', 'pipe_model.pkl'))
     model = joblib.load(model_path)
