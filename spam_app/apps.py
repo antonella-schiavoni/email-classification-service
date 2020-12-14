247f3363742e9aa416eed138342f85f123dbd52b
antonellaschiavoni <antonellaschiavoni2@gmail.com>
2020-12-14 14:51:43 -0300

    Delete local path and delete migrations config

--- spam_app/apps.py
+++ spam_app/apps.py
@@ -4,4 +4,4 @@ import joblib
 
 class SpamAppConfig(AppConfig):
     name = 'spam_app'
-    model = joblib.load('/Users/antonellaschiavoni/Antonella/Maestria/ProgramacionParaGrandesVolumenesDeDatos/tp-final/tp-final/spam/spam_app/spam-model/pipe_model.pkl')
+    #model = joblib.load('/Users/antonellaschiavoni/Antonella/Maestria/ProgramacionParaGrandesVolumenesDeDatos/tp-final/tp-final/spam/spam_app/spam-model/pipe_model.pkl')
